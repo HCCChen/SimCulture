a5da7cb7f0712aa7b6d9a2b95dd157262133c79b
HCCChen <ccx97u@gmail.com>
2015-09-03 19:33:54 +0800

    - Add user control template

--- lib/Calculate.cpp
+++ lib/Calculate.cpp
@@ -2,12 +2,12 @@
 
 //Create random number(integer) in given range
 int randomCreater(int from, int end){
-	return (from - end)*(double)rand()/RAND_MAX + end;
+    return (from - end)*(double)rand()/RAND_MAX + end;
 }
 //Give the happen/un-happen result base on given probability
 bool isHappen(double probability){
-	int proFlag = (int)(probability*100);
-	int happenFlag = randomCreater(1, 10000);
-	if(happenFlag <= proFlag){return true;}
-	else{return false;}
+    int proFlag = (int)(probability*100);
+    int happenFlag = randomCreater(1, 10000);
+    if(happenFlag <= proFlag){return true;}
+    else{return false;}
 }
--- sim.cpp
+++ sim.cpp
@@ -1,30 +1,94 @@
-/*
-	操作文化模擬進程
-*/
-
+#include <unistd.h>
 #include "lib/Culture.h"
 #include "lib/Terrain.h"
 #include "lib/Calculate.h"
 
+int commandHandler(char cmd);
+bool commandCatcher(char* cmd);
+void showMap(Terrain **map);
+
 int main(int argc, char* argv[]){
-	srand((unsigned)time(NULL));
-	Terrain terrain[20][20];
-	int i,j;
-	Culture culture[3];
+    srand((unsigned)time(NULL));
+    Terrain **terrain;
+    int i, j, ret, goingRound = 0;
+    char cmd;
+    Culture culture[3];
 
-	//Create Map
-	for(i = 0; i < 20; i++){
-		for(j = 0; j < 20; j++){
-			terrain[i][j].generalTerrian('c');
-		}
-	}
+    //Create Map
+    terrain = new Terrain*[20];
+    for(i = 0; i < 20; i++){
+        terrain[i] = new Terrain[20];
+        for(j = 0; j < 20; j++){
+            terrain[i][j].generalTerrian('.');
+        }
+    }
+    cout << "Map gereate finish" << endl;
 
-	for(i = 0; i < 20; i++){
-		for(j = 0; j < 20; j++){
-			cout << terrain[i][j].getTerrianType() << " ";
-		}
-		cout << endl;
-	}
+    while(1) {
+        //Show map
+        showMap(terrain);
+        //Handle user command
+        if (goingRound == 0) {
+            commandCatcher(&cmd);
+            ret = commandHandler(cmd);
+            if (ret == -1) {break;}
+            else if (ret == 1) {continue;}
+            else if (ret > 1) {
+                goingRound = ret;
+            }
+        }
+        else {
+            cout << "Round: " << goingRound << endl;
+            goingRound--;
+            sleep(1);
+        }
+    }
+    return 0;
+}
 
-	return 0;
+void showMap(Terrain **map) {
+    int i, j, ret;
+    ret = system("clear");
+    for(i = 0; i < 20; i++){
+        for(j = 0; j < 20; j++){
+            cout << map[i][j].getTerrianType() << " ";
+        }
+        cout << endl;
+    }
+}
+
+bool commandCatcher(char* cmd) {
+    cout << "Please enter the command: ";
+    cin >> cmd;
+    return true;
+}
+
+int commandHandler(char cmd) {
+    int intBuf = 0;
+    while(1) {
+        switch(cmd) {
+            case 'h':
+                cout << "Show command list: " << endl;
+                commandCatcher(&cmd);
+                continue;
+                break;
+            case 'E':
+                cout << "Exit the program" << endl;
+                return -1;
+                break;
+            case 'g':
+                cout << "Go to next round" << endl;
+                return 1;
+                break;
+            case 'G':
+                cout << "Go to next N round" << endl << "n: ";
+                cin >> intBuf;
+                return intBuf;
+                break;
+            default:
+                cout << "Invaild command, please try again: ";
+                cin >> cmd;
+                continue;
+        }
+    }
 }
